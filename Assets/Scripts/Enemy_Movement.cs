b4a174d4cb11c282b2b487c0465e011433578da9
Nguyen-Duc-Chien <204695864+Nguyen-Duc-Chien@users.noreply.github.com>
2025-09-22 01:23:37 +0700

    #9: Making Smarter Enemies
    - Làm quái chuyển scale khi di chuyển theo hướng tương ứng: Flip
    - Thêm các animation mới cho quái: Idle, Moving

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -3,24 +3,40 @@ using UnityEngine;
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
-    private bool isChasing;
+    private int facingDirection = -1;
+    private EnemyState enemyState;
 
     private Rigidbody2D rb;
     private Transform player;
+    private Animator anim;
 
-
+    //Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (isChasing == true)
+        if (enemyState == EnemyState.Chasing)
         {
+            if(player.position.x < transform.position.x && facingDirection == -1||
+                player.position.x > transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
             Vector2 direction = (player.position - transform.position).normalized;
             rb.linearVelocity = direction * speed;
         }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,7 +47,7 @@ public class Enemy_Movement : MonoBehaviour
             {
                 player = collision.transform;
             }
-            isChasing = true;
+            ChangeState(EnemyState.Chasing);
         }
     }
 
@@ -39,8 +55,33 @@ public class Enemy_Movement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            isChasing = false;
-            rb.linearVelocity = Vector2.zero; // Stop moving when player exits the trigger
+            rb.linearVelocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+            //Debug.Log("Set Idle: " + anim.GetBool("isIdle"));
         }
     }
+
+    void ChangeState(EnemyState newState)
+    {
+        //Exit the current animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", false);
+
+        //Update our current state
+        enemyState = newState;
+
+        //Update the new animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", true);
+    }
+}
+
+public enum EnemyState
+{
+    Idle,
+    Chasing,
 }
