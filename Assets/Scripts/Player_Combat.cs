d25833cbed2cfe3939539b6c089f57b83e470f8c
Nguyen-Duc-Chien <204695864+Nguyen-Duc-Chien@users.noreply.github.com>
2025-09-25 17:31:21 +0700

    Damaging Enemies: Action RPG in Unity Tutorial #13
    Người chơi có thể tấn công lại quái, y hệt việc quái làm vs người chơi :)

--- Assets/Scripts/Player_Combat.cs
+++ Assets/Scripts/Player_Combat.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Player_Combat : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float weaponRange = 1;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
     public Animator anim;
 
     public float cooldown;
@@ -23,8 +28,24 @@ public class Player_Combat : MonoBehaviour
         }
     }
 
+    public void DealDamage()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+        if (enemies.Length > 0)
+        {
+            enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-damage);
+        }
+    }
+
     public void FinishAttacking()
     {
         anim.SetBool("isAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
     }
 }
