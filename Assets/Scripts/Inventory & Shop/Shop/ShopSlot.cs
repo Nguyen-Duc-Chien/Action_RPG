312bd58b0e9f3c44c1a0f7a288153a4fca410ee2
Nguyen-Duc-Chien <204695864+Nguyen-Duc-Chien@users.noreply.github.com>
2026-04-18 16:52:07 +0700

    Shop System (Part VII): Fill the Shop with Items
    Thêm các item được cho từ Interface đã có sẵn ở phần trc

---
+++ Assets/Scripts/Inventory & Shop/Shop/ShopSlot.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Image itemImage;
+
+    private int price;
+
+    public void Initialized(ItemSO newItemSO, int price)
+    {
+        //fill the slot with information
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.itemName;
+        this.price = price;
+        priceText.text = price.ToString();
+    }
+}
